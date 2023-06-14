3312e3757f3ff31264769adc1ca2f8489b7d2018
IsiteYves <yvesisite@gmail.com>
2023-06-14 21:35:24 +0200

    ft-added swagger-autogen for automatic API documentation generation

--- app.js
+++ app.js
@@ -2,17 +2,22 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 require("./config/database").connect();
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger-output.json')
 
 const app = express();
 
 // configure cors
 app.use(cors());
-
+const PORT = process.env.PORT || 8080;
+const host = process.env.HOST || "localhost";
 // parse requests of content-type - application/json
 app.use(express.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Supamenu APIs!" });
--- config/database.js
+++ config/database.js
@@ -1,17 +1,19 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const { MONGO_URI } = process.env;
 exports.connect = () => {
-    mongoose.connect(MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        
-    }).then(() => {
-        console.log("Successfully connected to database");
-    }).catch((error) => {
-        console.log("db conn faild");
-        console.error(error);
-        process.exit(1)
+  mongoose
+    .connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
-}
+    .then(() => {
+      console.log("Successfully connected to database");
+    })
+    .catch((error) => {
+      console.log("mongodb connection failed");
+      console.error(error);
+      process.exit(1);
+    });
+};
